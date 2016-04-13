74e9aebacddf414ba5fda6131fa5be0a48ef81fe
csato <cristine.sato@gmail.com>
2016-04-13 20:26:08 -0300

    Added files via upload

---
+++ NotasTurmaLogica.java
@@ -0,0 +1,40 @@
+class NotasTurmaLogica{
+
+	public static void main (String[] args){
+		
+		String [] nomes = new String[10];
+
+		float[] notas = new float [10];
+
+		System.out.println("Nomes " + nomes);
+
+		System.out.println("notas "+ notas);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
