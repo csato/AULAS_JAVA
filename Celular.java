6bbfd2c96bbf558b0201ff7763cf5e0ebdfda45d
csato <cristine.sato@gmail.com>
2016-04-18 21:51:19 -0300

    Added files via upload

---
+++ Celular.java
@@ -0,0 +1,18 @@
+
+class Celular {
+	
+	String processador;
+	float tamanho;
+	String cor;
+	float memoriaRam;
+	float tamanhoTela;
+	String bateria;
+	int qualidadeSom;
+	String fabricante;
+	String modelo;
+	String nome;
+	String sistemaOperacional;
+	
+	
+
+}
