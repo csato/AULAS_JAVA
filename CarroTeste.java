f4c1d097aef14ea3573cfe18825442d1d5b03227
Cristine Sato <cristine.sato@gmail.com>
2016-04-25 20:51:14 -0300

    sssss
    ddddd

---
+++ CarroTeste.java
@@ -0,0 +1,11 @@
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Carro();
+		
+
+	}
+
+}
---
+++ Test2.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+
+public class Test2 extends TestCase {
+
+	public void testLigar() {
+		fail("Not yet implemented");
+	}
+
+	public void testAcelerar() {
+		fail("Not yet implemented");
+	}
+
+	public void testFrear() {
+		fail("Not yet implemented");
+	}
+
+}
