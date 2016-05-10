cd1d241d2a6cef673de324fbd7c06c634fa44e24
Cristine Sato <cristine.sato@gmail.com>
2016-05-09 21:37:23 -0300

    Add files via upload

---
+++ aj2/exemplo1/src/exemplo1/Exemplo.java
@@ -0,0 +1,20 @@
+package exemplo1;
+
+public class Exemplo {
+ public static void main(String[] args) {
+	
+
+		// TODO Auto-generated method stub
+		Exemplo exemplo = new Exemplo();
+		int resultado = soma(5, 8);
+
+
+ }
+
+
+public static int soma(int i, int j) {
+	 return i+j;
+	}
+
+
+}
