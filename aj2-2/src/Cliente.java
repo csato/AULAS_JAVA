6046b0ce72655895db9fb4301f259af8246381d6
Cristine Sato <cristine.sato@gmail.com>
2016-05-16 20:56:40 -0300

    Add files via upload

---
+++ aj2-2/src/Cliente.java
@@ -0,0 +1,29 @@
+
+		public class Cliente {
+			private String nome;
+			private int idade;
+			
+			public String getNome() {
+				return nome;
+			}
+
+			public void setNome(String nome) {
+				this.nome = nome;
+			}
+
+			public int getIdade() {
+				return idade;
+			}
+
+			public void setIdade(int idade) {
+				this.idade = idade;
+			}
+			
+			public void fala(String algumaCoisa) {
+				
+				String fala = algumaCoisa;
+				//String fala = "Olá";
+				System.out.println(fala);
+				
+			}
+}
