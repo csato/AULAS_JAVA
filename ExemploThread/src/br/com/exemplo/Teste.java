5b81d525e19db0041b4f6a98cca9847a7d9e580d
Cristine Sato <cristine.sato@gmail.com>
2016-06-29 22:20:33 -0300

    Add files via upload

--- ExemploThread/src/br/com/exemplo/Teste.java
+++ ExemploThread/src/br/com/exemplo/Teste.java
@@ -1,12 +1,21 @@
 package br.com.exemplo;
 
-import br.com.exemplothread.Printer;
 
 public class Teste {
 
 	public static void main(String[] args) {
 	
-
+		Printer p1 = new Printer('A');
+		Printer p2 = new Printer('-');
+		Printer p3 = new Printer('&');
+		
+		Thread t1 = new Thread(p1);
+		Thread t2 = new Thread(p2);
+		Thread t3 = new Thread(p3);
+		
+		t1.start();
+		t2.start();
+		t3.start();
 		
 		
 	}
